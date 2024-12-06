647be4a568897fc1a65025874e2ae29e77c5b500
nonrita <2220025@ecc.ac.jp>
2024-12-06 09:35:18 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+const app: express.Express = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//CROS対応（というか完全無防備：本番環境ではだめ絶対）
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  }
+);
+
+app.listen(3001, () => {
+  console.log('Start on port 3001.');
+});
+
+app.get('/users', (req: express.Request, res: express.Response) => {
+  res.send('aaaa');
+});
