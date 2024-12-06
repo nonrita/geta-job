3e1ab1b93faa720d10cb528925fd00b8df1a29ce
nonrita <2220025@ecc.ac.jp>
2024-12-06 10:04:36 +0900

    change: cors対策をライブラリに変えました

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const app: express.Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//CROS対応（というか完全無防備：本番環境ではだめ絶対）
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    next();
-  }
-);
+app.use(cors());
 
 app.listen(3001, () => {
   console.log('Start on port 3001.');
